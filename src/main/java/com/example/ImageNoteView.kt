346641ca560194945d25fb1c95826ea8a3bea48a
toshinoritakai <toshinori.takai@change-vision.com>
2021-06-04 11:40:27 +0900

    ノートや画像の移動に応じて自動的に位置を合わせるようにした

--- src/main/java/com/example/ImageNoteView.kt
+++ src/main/java/com/example/ImageNoteView.kt
@@ -1,5 +1,7 @@
 package com.example
 
+import com.change_vision.jude.api.inf.AstahAPI
+import com.change_vision.jude.api.inf.presentation.IPresentation
 import com.change_vision.jude.api.inf.project.ProjectEvent
 import com.change_vision.jude.api.inf.project.ProjectEventListener
 import com.change_vision.jude.api.inf.ui.IPluginExtraTabView
@@ -16,10 +18,11 @@ class ImageNoteView : JPanel(), IPluginExtraTabView, ProjectEventListener, Actio
     val buttonAssociate = ButtonAssociate()
     val buttonRefreashBasedOnNote = ButtonRefresh(RefreshBaseType.NOTE)
     val buttonRefreashBasedOnImage = ButtonRefresh(RefreshBaseType.IMAGE)
+    var isAlinging: Boolean = false
 
     init {
         val panelButtons = JPanel()
-        panelButtons.layout = GridLayout(1,2)
+        panelButtons.layout = GridLayout(1, 2)
         panelButtons.add(buttonAssociate)
         panelButtons.add(buttonRefreashBasedOnNote)
         panelButtons.add(buttonRefreashBasedOnImage)
@@ -27,6 +30,16 @@ class ImageNoteView : JPanel(), IPluginExtraTabView, ProjectEventListener, Actio
         buttonRefreashBasedOnNote.addActionListener(this)
         buttonRefreashBasedOnImage.addActionListener(this)
         add(panelButtons)
+        addProjectEventListener()
+    }
+
+    private fun addProjectEventListener() {
+        try {
+            val projectAccessor = AstahAPI.getAstahAPI().projectAccessor
+            projectAccessor.addProjectEventListener(this)
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+        }
     }
 
     override fun actionPerformed(e: ActionEvent) {
@@ -42,6 +55,7 @@ class ImageNoteView : JPanel(), IPluginExtraTabView, ProjectEventListener, Actio
             }
         }
     }
+
     override fun getTitle(): String = "Image Note"
     override fun getDescription(): String = "Image Note View"
     override fun getComponent(): Component = this
@@ -50,5 +64,20 @@ class ImageNoteView : JPanel(), IPluginExtraTabView, ProjectEventListener, Actio
     override fun deactivated() {}
     override fun projectOpened(p0: ProjectEvent?) {}
     override fun projectClosed(p0: ProjectEvent?) {}
-    override fun projectChanged(p0: ProjectEvent?) {}
+    override fun projectChanged(p0: ProjectEvent?) {
+        if (!isAlinging) {
+            p0?.let { projectEvent ->
+                val presentationUnits = projectEvent.projectEditUnit.map { it.entity }.filterIsInstance<IPresentation>()
+                if (presentationUnits.any { it.type == "Note" }) {
+                    isAlinging = true
+                    buttonRefreashBasedOnNote.push(AstahAccessor.getCurrentDiagram())
+                    isAlinging = false
+                } else if (presentationUnits.any { it.type == "Image" }) {
+                    isAlinging = true
+                    buttonRefreashBasedOnImage.push(AstahAccessor.getCurrentDiagram())
+                    isAlinging = false
+                }
+            }
+        }
+    }
 }
